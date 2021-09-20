c5d79c20d3a262d224adf3a79d7fbfac7b95dbc5
Mikele91 <michele.amoruso91.8@gmail.com>
2021-09-20 15:32:36 +0200

    aggiunta remove todo

--- main.js
+++ main.js
@@ -4,8 +4,19 @@ const app = new Vue ({
         todos:[
             "Fare i compiti", "Fare il check in", "Prednere l'aereo"
         ],
+        newTodo:"",
     },
     methods:{
+        push: function(){
+            if(this.newTodo!= ""){
+                this.todos.push(this.newTodo);
+                this.newTodo="";
+
+            }
+        },
+        removeTodo: function(todo_index){
+            this.todos.splice(todo_index, 1);
+        }
 
     }
 })
