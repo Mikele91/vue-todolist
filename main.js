7cce13ac22238e9ac0917d1dbf9b9417476ec565
Mikele91 <michele.amoruso91.8@gmail.com>
2021-09-20 14:34:36 +0200

    aggiunta struttura base

---
+++ main.js
@@ -0,0 +1,11 @@
+const app = new Vue ({
+    el: "#root",
+    data:{
+        todos:[
+            "Fare i compiti", "Fare il check in", "Prednere l'aereo"
+        ],
+    },
+    methods:{
+
+    }
+})
